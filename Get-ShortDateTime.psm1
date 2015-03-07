94972786365484ab37250e8c9b69e89ebc35c340
Robert Biddle <Robert.D.Biddle@gmail.com>
2015-03-07 11:17:49 -0500

    New Module
    Get-ShortDateTime Module added

---
+++ Get-ShortDateTime.psm1
@@ -0,0 +1,16 @@
+﻿##########################################
+# Robert D. Biddle
+# Created - 03.07.2015
+    <#
+     
+    .SYNOPSIS
+    Module containing functions which output short Date/Time data
+    #>
+#
+Function Get-ShortDate{
+$Date = Get-Date;$Date.ToShortTimeString()
+}
+Function Get-SimpleTime{
+$Time = Get-Date;$Time.ToShortTimeString()
+}
+Export-ModuleMember -Function * -Variable *
