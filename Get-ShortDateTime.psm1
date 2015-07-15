03f405421332c2264bd696a712b6ba7e4c40dff1
Robert Biddle <Robert.D.Biddle@gmail.com>
2015-07-15 15:04:31 -0400

    Added option for file name friendly output.

--- Get-ShortDateTime.psm1
+++ Get-ShortDateTime.psm1
@@ -7,10 +7,38 @@
     Module containing functions which output short Date/Time data
     #>
 #
-Function Get-ShortDate{
-$Date = Get-Date;$Date.ToShortTimeString()
+Function Get-ShortDate
+{
+    Param
+    (
+        [Parameter(Mandatory=$false)]
+        [Switch]
+        $FilenameCompatibleFormat
+    )
+    if($FilenameCompatibleFormat)
+    {
+        $($Date = Get-Date;$Date.ToShortDateString()).Replace("/","-")
+    }
+    Else
+    {
+        $Date = Get-Date;$Date.ToShortDateString()
+    }
 }
-Function Get-ShortTime{
-$Time = Get-Date;$Time.ToShortTimeString()
+Function Get-ShortTime
+{
+    Param
+    (
+        [Parameter(Mandatory=$false)]
+        [Switch]
+        $FilenameCompatibleFormat
+    )
+    if($FilenameCompatibleFormat)
+    {
+        $($Time=get-date;$Time.ToShortTimeString()).Replace(":","-").Replace(" ","-")
+    }
+    Else
+    {
+        $Time = Get-Date;$Time.ToShortTimeString()
+    }
 }
 Export-ModuleMember -Function * -Variable *
